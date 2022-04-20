97937d1609b5b425b71b9a0662e9e045648fab02
Coela Can't! <coelacannot@gmail.com>
2022-04-19 21:55:13 -0400

    Added minor tweaks/fixes

--- GCODEGenerator.py
+++ GCODEGenerator.py
@@ -1,10 +1,10 @@
 import csv
 
-inputFilename = "PickPlaceKV4_Top.csv"
+inputFilename = "PickPlaceKV4_Bottom.csv"
 outputFilename = "PickPlaceOutput.gcode"
 
 feederRotationOffset = -180.0 #forward or backward rotation depending on feeder location
-feederMMPerComponent = -13.33 #requires additional 0.05 every 15 to normalize rotation
+feederMMPerComponent = 10.00 #requires additional 0.05 every 15 to normalize rotation
 fAxisMMPerDegree = 200.0/360.0 #200MM movement is 360 degree rotation
 fAxisInvertRotation = 1.0 #Multiplies by input rotation to flip direction
 
@@ -17,17 +17,19 @@ nozzleSlowMove = 3000
 
 xyTravelZAxisHeight = 98.0 # Height for traveling with xy moves
 
-feederXPosition = 478.0
-feederYPosition = 761.9
+feederXPosition = 480.6
+feederYPosition = 763.6
 feederZPosition = 87.0
 feederSlowDownOffset = 2.0 #offset from z position where movement is slowed down
-feederSynchronization = -40.0 + (13.33 * 3.0) # for syncrhonizing offset every 5 components
-feederSyncCounter = 0 # Current Sync Counter
-feederSyncMax = 4 # Once counter is at max add synchronization to mm per component
 feederForwardPark = 80.0 # Park in front of feeder prior to picking up
 
-pcbXPosition = 258.10 - 21.5
-pcbYPosition = 554.50 - 2.5
+#VERTICAL
+#pcbXPosition = 258.40 - 21.5 - 0.55
+#pcbYPosition = 554.90 - 2.5 - 0.225
+#HORIZONTAL
+pcbXPosition = 397.4 + 21.5 - 0.55
+pcbYPosition = 554.90 - 2.5 - 0.225
+
 pcbZPosition = 52.0
 pcbSlowDownOffset = 6.0 # Offset from z position where movement is slowed down
 
@@ -35,8 +37,8 @@ def S(value):
     return str(round(value, 3))
 
 def GetAcceleration():
-    output = "M201 X2500 Y2500 Z2500\n"
-    output += "M204 P1000 T1000\n"
+    output = "M201 X2500 Y1500 Z1000\n"
+    output += "M204 P3000 T3000\n"
     return output
 
 def GetTrammingLine():
@@ -50,25 +52,16 @@ def GetHomingLine():
     return "G28 XYZ\n"
 
 def GetAirOnLine():
-    return "M400\nM42 P36 S255\nM42 P24 S255\nG4 P250\n" # Vacuum on, valve open, pause
+    return "M42 P36 S255\nM42 P24 S255\nM400\n" # Vacuum on, valve open, pause
     
 def GetAirOffLine():
-    return "M400\nM42 P36 S0\nM42 P24 S0\nG4 P250\n" # Vacuum off, valve closed, pause
+    return "M400\nM42 P24 S0\nM42 P36 S0\nG4 P500\n" # Vacuum off, valve closed, pause
 
 def GetComponentLine(xPosition, yPosition, rotation):
-    global feederSyncCounter
     output = ""
 
     feederDistance = feederMMPerComponent
 
-    if feederSyncCounter < feederSyncMax:
-        feederSyncCounter += 1
-    else:
-        feederSyncCounter = 0
-        feederDistance += feederSynchronization # Add syncrhonization distance to feeder if out of sync
-
-
-    output += "M400\n" # Wait until finished then continue
     output += "T1\n" # Switch to feeder spin
     output += "G92 E0\n" # Set relative e position
 
@@ -76,8 +69,9 @@ def GetComponentLine(xPosition, yPosition, rotation):
     output += "G0 X" + S(feederXPosition) + " Y" + S(feederYPosition) + " F" + S(xyFastMove) + "\n" # Move to Feeder XY
 
     output += "G0 Z" + S(feederZPosition + feederSlowDownOffset) + " F" + S(nozzleFastMove) + "\n" # Nozzle drop fast move
+    
+    output += "M400\n" # Wait until finished then continue
     output += "G0 Z" + S(feederZPosition) + " F" + S(nozzleSlowMove) + "\n" # Nozzle drop finishing move slow
-
     output += GetAirOnLine()
     
     output += "G0 Z" + S(xyTravelZAxisHeight) + " F" + S(zFastMove) + "\n" # Move z up for travel
@@ -87,11 +81,11 @@ def GetComponentLine(xPosition, yPosition, rotation):
     output += "T0\n" # Switch to nozzle spin
     output += "G92 E0\n" # Set relative e position
     output += "G0 X" + S(pcbXPosition + xPosition) + " Y" + S(pcbYPosition + yPosition) + " Z" + S(pcbZPosition + pcbSlowDownOffset) + " E" + S(fAxisMMPerDegree * (rotation + feederRotationOffset) * fAxisInvertRotation) + " F" + S(xyFastMove) + "\n" # Move to PCB coordinate
+   
     output += "G0 Z" + S(pcbZPosition) + " F" + S(zSlowMove) + "\n" # Z Axis slow move to drop component
-
     output += GetAirOffLine()
 
-    output += "G0 Z" + S(pcbZPosition + pcbSlowDownOffset) + " F" + S(xyFastMove) + "\n" # Move to clearance height for Z move
+    output += "G0 Z" + S(pcbZPosition + pcbSlowDownOffset) + " F" + S(zSlowMove) + "\n" # Move to clearance height for Z move
  
     return output
 
